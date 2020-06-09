e6467c9ea45c48995ad106a9478cbea9500297cb
simplelii <simplelii@163.com>
2020-06-09 15:08:52 +0800

    初始化工程

---
+++ src/test/java/com/simplelii/springbootstage/SpringbootStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplelii.springbootstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
