9dd85c01df5133bdb7483f2b144900d7b09da14f
simplelii <simplelii@163.com>
2020-06-16 18:11:40 +0800

    配置文件封装VO-2

--- springboot-stageservice/src/main/java/com/simplelii/app/service/config/registryvo/TestPropertiesVo.java
+++ springboot-stageservice/src/main/java/com/simplelii/app/service/config/registryvo/TestPropertiesVo.java
@@ -1,29 +1,19 @@
 package com.simplelii.app.service.config.registryvo;
 
 import lombok.Data;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 /**
  * @author liXin
  * @description
  * @date 2020/6/16
  */
+@Data
 @Component
 @ConfigurationProperties(prefix = "redis")
-@Data
-public class TestPropertiesVo implements InitializingBean {
+public class TestPropertiesVo {
     private String test;
 
-    @Resource
-    private TestPropertiesByPropertiesVo vo;
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("2121");
-
-    }
 }
